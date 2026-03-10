0bf85c2fe0372b2301690a6704de36e3e2f94a44
ykngs1841 <122946329+ykngs1841@users.noreply.github.com>
2026-03-10 16:38:08 +0900

    init devops project

--- game-server/server.py
+++ game-server/server.py
@@ -1,13 +1,25 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from prometheus_client import Counter, generate_latest
+
+REQUEST_COUNT = Counter("game_requests_total", "Total Game Requests")
 
 class GameHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+
         if self.path == "/health":
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"OK")
+
+        elif self.path == "/metrics":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(generate_latest())
+
         else:
+            REQUEST_COUNT.inc()
             self.send_response(404)
             self.end_headers()
 
